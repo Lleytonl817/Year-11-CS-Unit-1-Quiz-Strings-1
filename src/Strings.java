49a6daf9d4acde6ab384d5d3c6572d56196e017c
Lleyton <ldl385@student.cis.edu.hk>
2023-02-20 09:50:08 +0800

    completed

--- src/Strings.java
+++ src/Strings.java
@@ -3,24 +3,30 @@ public class Strings {
     public static void main(String[] args) {
 
         // 1.
+        int age = 33;
 
         // 2.
+        String name = new String("T Swizzle");
 
         // 3.
-
+        System.out.println(age);
         // 4.
-
+        System.out.println(name);
         // 5.
-
+        String description = new String("My name is " + name + " and I am " + age + " years old");
         // 6.
-
+        System.out.println(description);
         // 7.
-
+        System.out.println(description.length());
         // 8.
+        String nameSubDescription = new String(description.substring(11, 21));
 
         // 9.
-
+        int ageSubDescription = Integer.parseInt(description.substring(30,32));
         // 10.
+
+        String subDescription = new String("My name is " + nameSubDescription + " and I am " + ageSubDescription + " years old");
+        System.out.println(subDescription);
 
     }
 
